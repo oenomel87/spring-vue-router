7f71490397d64917eba0fbf4f4d6bcc38dc31f1c
dane-park <dane.park@kakaocorp.com>
2021-06-19 23:18:18 +0900

    create repo

---
+++ vue/vue.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+
+    outputDir: path.resolve(__dirname, '../src/main/resources/static/vue'),
+
+    publicPath: '/vue',
+
+    indexPath: '../../templates/vue/index.html'
+}
